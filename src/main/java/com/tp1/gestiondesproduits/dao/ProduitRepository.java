0ce7b7e7bd8f6e4d64e6055d694dfe8ecbe6bab5
benzina <benzina.elmahdi@gmail.com>
2021-09-11 18:20:46 +0100

    ajout de spring data rest

--- src/main/java/com/tp1/gestiondesproduits/dao/ProduitRepository.java
+++ src/main/java/com/tp1/gestiondesproduits/dao/ProduitRepository.java
@@ -1,8 +1,20 @@
 package com.tp1.gestiondesproduits.dao;
 
 import com.tp1.gestiondesproduits.entity.Produit;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
+import java.util.List;
+
+@RepositoryRestResource
 public interface ProduitRepository extends JpaRepository<Produit,Long> {
+    @RestResource(path = "/byDesignation")
+    public List<Produit> findProduitsByDesignationContains(@Param("mc") String des);
+    @RestResource(path = "/byDesignationPage")
+    public Page<Produit> findProduitsByDesignationContains(@Param("mc") String des, Pageable pageable);
 
 }
