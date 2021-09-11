5281192de3def0b852677276384f4b302f2f4a7b
benzina <benzina.elmahdi@gmail.com>
2021-09-11 16:52:57 +0100

    premier commit : le projet initial

---
+++ src/main/java/com/tp1/gestiondesproduits/GestionDesProduitsApplication.java
@@ -0,0 +1,13 @@
+package com.tp1.gestiondesproduits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDesProduitsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionDesProduitsApplication.class, args);
+    }
+
+}
