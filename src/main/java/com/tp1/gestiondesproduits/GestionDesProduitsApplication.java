fb8776388df68573da3c916a17e713c68c7ce688
benzina <benzina.elmahdi@gmail.com>
2021-09-11 19:11:56 +0100

    affichage de idProduit en fichier json

--- src/main/java/com/tp1/gestiondesproduits/GestionDesProduitsApplication.java
+++ src/main/java/com/tp1/gestiondesproduits/GestionDesProduitsApplication.java
@@ -6,11 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 @SpringBootApplication
 public class GestionDesProduitsApplication implements CommandLineRunner {
     @Autowired
     private ProduitRepository produitRepository;
+    @Autowired
+    private RepositoryRestConfiguration restConfiguration;
 
     public static void main(String[] args) {
         SpringApplication.run(GestionDesProduitsApplication.class, args);
@@ -18,6 +21,7 @@ public class GestionDesProduitsApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        restConfiguration.exposeIdsFor(Produit.class);
         produitRepository.save(new Produit(null,"PC accer",3887,2));
         produitRepository.save(new Produit(null,"PC samsung",6887,12));
 
